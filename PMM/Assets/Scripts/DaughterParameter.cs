3c6cff14c0314ab5baf1e449684c91420e9f3279
Youngsang <elbry92@gmail.com>
2018-05-26 23:24:18 +0900

    parameter 관련 스크립트와 오브젝트 생성, 하위 폴더(Scenes, Scripts) 생성

---
+++ PMM/Assets/Scripts/DaughterParameter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DaughterParameter : MonoBehaviour {
+
+	int strength;
+	int intelligence;
+	int dignity;
+	int sense;
+
+	// Use this for initialization
+	void Start () {
+		Initialize();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Initialize()
+	{
+
+	}
+}
---
+++ PMM/Assets/Scripts/FatherParameter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FatherParameter : MonoBehaviour {
+
+public enum fatherDream{nonexistant, princessMaking};
+
+	int strength;
+	int intelligence;
+	int dignity;
+	int sense;
+	int stress;
+	int will;
+
+	fatherDream firstDream;
+	fatherDream secondDream;
+
+	// Use this for initialization
+	void Start () {
+		Initialize();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Initialize()
+	{
+
+	}
+
+}
